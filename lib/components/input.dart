52479084a209662bde66553b4bd11d21554b6757
sunny-github-acc <54981869+sunny-github-acc@users.noreply.github.com>
2024-10-26 07:56:11 +0300

    Update input.dart

--- lib/components/input.dart
+++ lib/components/input.dart
@@ -1,73 +1,350 @@
+import 'package:challenges/components/app_bar.dart';
+import 'package:challenges/components/button.dart';
+import 'package:challenges/components/circular_progress_indicator.dart';
 import 'package:challenges/components/column.dart';
-import 'package:challenges/utils/colors.dart';
-import 'package:flutter/material.dart';
-
+import 'package:challenges/components/container_gradient.dart';
+import 'package:challenges/components/date.dart';
+import 'package:challenges/components/dropdown.dart';
+import 'package:challenges/components/input.dart';
+import 'package:challenges/components/modal.dart';
+import 'package:challenges/components/row.dart';
 import 'package:challenges/components/text.dart';
-import 'package:flutter/services.dart';
+import 'package:challenges/logic/bloc/auth/auth_bloc.dart';
+import 'package:challenges/logic/bloc/auth/auth_state.dart';
+import 'package:challenges/logic/bloc/collections/collections_bloc.dart';
+import 'package:challenges/logic/bloc/collections/collections_events.dart';
+import 'package:challenges/logic/bloc/collections/collections_state.dart';
+import 'package:challenges/logic/bloc/tribes/tribes_bloc.dart';
+import 'package:challenges/logic/bloc/tribes/tribes_events.dart';
+import 'package:challenges/logic/bloc/tribes/tribes_state.dart';
+import 'package:challenges/services/auth/auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class CustomInput extends StatelessWidget {
-  final TextEditingController controller;
-  final TextInputType keyboardType;
-  final String? title;
-  final String? labelText;
-  final String? hintText;
-  final bool isAutocorrect;
-  final bool isDisabled;
-  final bool isObscureText;
-  final bool isSuggestions;
-  final bool isTall;
-  final int? limit;
-  final void Function(String)? onChanged;
+class CreateChallenge extends StatefulWidget {
+  const CreateChallenge({super.key});
 
-  const CustomInput({
-    super.key,
-    required this.controller,
-    this.title,
-    this.labelText,
-    this.hintText,
-    this.isAutocorrect = false,
-    this.isDisabled = false,
-    this.isObscureText = false,
-    this.isSuggestions = true,
-    this.isTall = false,
-    this.keyboardType = TextInputType.text,
-    this.limit,
-    this.onChanged,
-  });
+  @override
+  State<CreateChallenge> createState() => _CreateChallenge();
+}
+
+class _CreateChallenge extends State<CreateChallenge> {
+  final TextEditingController titleController = TextEditingController();
+  final TextEditingController descriptionController = TextEditingController();
+  final TextEditingController consequenceController = TextEditingController();
+  final AuthService auth = AuthService();
+
+  late List<String> titles;
+
+  String consequence = '';
+  String duration = 'Week';
+  String visibility = 'public';
+  bool isTitle = true;
+  bool isDuration = true;
+  DateTime today = DateTime.now();
+  DateTime customStartDate = DateTime.now();
+  DateTime customEndDate = DateTime.now().add(const Duration(days: 7));
+
+  int titleLimit = 50;
+  int descriptionLimit = 300;
+  int consequenceLimit = 300;
+  String titleLabelShort = 'Make it short and sweet';
+  String descriptionLabelShort = 'Be as specific as you can';
+  String consequenceLabelShort =
+      'What will happen if your challenge succeeds or fails?';
+  late String titleLabelLong;
+  late String descriptionLabelLong;
+  late String consequenceLabelLong;
+  late String titleLabel;
+  late String descriptionLabel;
+  late String consequenceLabel;
+
+  @override
+  void initState() {
+    super.initState();
+
+    User? user = BlocProvider.of<AuthBloc>(context).state.user;
+
+    if (user != null) {
+      BlocProvider.of<TribesBloc>(context).add(
+        TribesEventGetTribes(
+          user: user,
+        ),
+      );
+    }
+
+    titleLabelLong = '$titleLabelShort (Reached limit $titleLimit)';
+    descriptionLabelLong =
+        '$descriptionLabelShort (Reached limit $descriptionLimit)';
+    consequenceLabelLong =
+        '$consequenceLabelShort (Reached limit $consequenceLimit)';
+    titleLabel = titleLabelShort;
+    descriptionLabel = descriptionLabelShort;
+    consequenceLabel = consequenceLabelShort;
+  }
+
+  Future<void> save(context) async {
+    String title = titleController.text.trim();
+    String description = descriptionController.text.trim();
+    String consequence = consequenceController.text.trim();
+    Map user = auth.getUser();
+
+    DateTime endDate = getEndDate(duration, customEndDate);
+
+    Map<String, dynamic> document = {
+      ...user,
+      'title': title,
+      'description': description,
+      'createdAt': today,
+      'startDate': customStartDate,
+      'endDate': endDate,
+      'duration': duration,
+      'consequence': consequence,
+      'visibility': visibility,
+      'isFinished': false,
+    };
+
+    setState(() {
+      isTitle = title.isNotEmpty;
+    });
+
+    if (!isTitle || !isDuration) {
+      return Modal.show(context, 'Oops', 'Please fill out all input fields');
+    }
+
+    BlocProvider.of<CollectionsBloc>(context).add(
+      CollectionsEventAddCollection(
+        title: 'challenges',
+        document: document,
+      ),
+    );
+  }
+
+  void handleDuration(BuildContext context, String durationParam) {
+    setState(() {
+      duration = durationParam;
+    });
+  }
+
+  void handleVisibility(String visibilityParam) {
+    Map user = auth.getUser();
+    Map<String, String> visibilityMap = {
+      'Everyone': 'public',
+      'Only me': user['uid'],
+    };
+
+    setState(() {
+      visibility = visibilityMap[visibilityParam] ?? visibilityParam;
+    });
+  }
+
+  DateTime getEndDate(duration, customDate) {
+    if (duration == 'Custom') {
+      return customDate;
+    } else {
+      return DateTime.utc(
+        today.year + (duration == 'Year' ? 1 : 0),
+        today.month + (duration == 'Month' ? 1 : 0),
+        today.day + (duration == 'Week' || duration == 'Infinite' ? 7 : 0),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return CustomColumn(
-      spacing: SpacingType.small,
-      children: [
-        if (title != null) CustomText(text: title!),
-        SizedBox(
-          height: isTall ? 150 : null,
-          child: TextFormField(
-            controller: controller,
-            decoration: InputDecoration(
-              labelText: labelText,
-              hintText: hintText,
-              enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(
-                  color: isDisabled ? colorMap['red']! : colorMap['blue']!,
-                  width: 1.0,
-                ),
-              ),
-              alignLabelWithHint: true,
+    Map user = auth.getUser();
+
+    return BlocListener<CollectionsBloc, CollectionsState>(
+      listener: (context, state) {
+        if (kDebugMode) {
+          print(
+              '🚀 CreateChallenge BlocListener CollectionsBloc state: $state');
+        }
+
+        if (state.success != null) {
+          ScaffoldMessenger.of(context).hideCurrentSnackBar();
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: CustomText(text: state.success!),
+              duration: const Duration(seconds: 2),
             ),
-            maxLines: isTall ? 10 : 1,
-            keyboardType: keyboardType,
-            obscureText: isObscureText,
-            autocorrect: isAutocorrect,
-            enableSuggestions: isObscureText ? false : isSuggestions,
-            inputFormatters: limit != null
-                ? [LengthLimitingTextInputFormatter(limit)]
-                : null,
-            onChanged: onChanged,
+          );
+
+          Navigator.pop(context);
+        }
+
+        if (state.error != null) {
+          Modal.show(
+            context,
+            state.error!.dialogTitle,
+            state.error!.dialogText,
+          );
+        }
+      },
+      child: Scaffold(
+        appBar: CustomAppBar(
+          title: 'Create Challenge',
+          leftButton: BlocBuilder<CollectionsBloc, CollectionsState>(
+            builder: (context, state) {
+              if (state.isLoading) {
+                return const CustomCircularProgressIndicator();
+              }
+
+              return IconButton(
+                icon: const Icon(Icons.arrow_back),
+                onPressed: () => Navigator.pop(context),
+              );
+            },
+          ),
+          actions: [
+            BlocBuilder<CollectionsBloc, CollectionsState>(
+              builder: (context, state) {
+                return CustomButton(
+                  text: 'Save',
+                  size: ButtonSize.small,
+                  isLoading: state.isLoading,
+                  onPressed: () => save(context),
+                );
+              },
+            ),
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: CustomContainer(
+            child: CustomColumn(
+              spacing: SpacingType.medium,
+              children: [
+                CustomInput(
+                  title: 'Enter a title',
+                  labelText: titleLabel,
+                  controller: titleController,
+                  isDisabled: !isTitle,
+                  limit: titleLimit,
+                  onChanged: (e) => setState(() {
+                    titleLabel = titleController.text.length == titleLimit
+                        ? titleLabelLong
+                        : titleLabelShort;
+                  }),
+                ),
+                CustomInput(
+                  title: 'Describe your challenge',
+                  labelText: descriptionLabel,
+                  controller: descriptionController,
+                  isTall: true,
+                  limit: descriptionLimit,
+                  onChanged: (e) => setState(() {
+                    descriptionLabel =
+                        descriptionController.text.length == descriptionLimit
+                            ? descriptionLabelLong
+                            : descriptionLabelShort;
+                  }),
+                ),
+                CustomInput(
+                  title: 'Enter a consequence',
+                  labelText: consequenceLabel,
+                  controller: consequenceController,
+                  isTall: true,
+                  limit: consequenceLimit,
+                  onChanged: (e) => setState(() {
+                    consequenceLabel =
+                        consequenceController.text.length == consequenceLimit
+                            ? consequenceLabelLong
+                            : consequenceLabelShort;
+                  }),
+                ),
+                CustomColumn(
+                  spacing: SpacingType.small,
+                  children: [
+                    const CustomText(
+                      text: 'How long will your challenge last?',
+                    ),
+                    CustomDropdown(
+                      values: const [
+                        'Week',
+                        'Month',
+                        'Year',
+                        'Infinite',
+                        'Custom',
+                      ],
+                      hint: 'Select an option',
+                      value: duration,
+                      onChanged: (dynamic value) {
+                        handleDuration(context, value as String);
+                      },
+                    ),
+                    if (duration == 'Custom')
+                      CustomDateRangePicker(
+                        dateRange: DateTimeRange(
+                          start: customStartDate,
+                          end: customEndDate,
+                        ),
+                        onSelected: (date) {
+                          setState(() {
+                            customStartDate = date.start;
+                            customEndDate = date.end;
+                          });
+                        },
+                      ),
+                  ],
+                ),
+                CustomColumn(
+                  spacing: SpacingType.small,
+                  children: [
+                    const CustomText(
+                      text: 'Who can see your challenge?',
+                    ),
+                    CustomRow(
+                      spacing: SpacingType.medium,
+                      children: [
+                        BlocBuilder<TribesBloc, TribesState>(
+                          builder: (context, state) {
+                            if (state.isLoading) {
+                              return const CustomCircularProgressIndicator(
+                                scale: 0.5,
+                              );
+                            }
+
+                            return CustomDropdown(
+                              values: [
+                                'Everyone',
+                                'Only me',
+                                ...state.tribes,
+                              ],
+                              hint: 'Select an option',
+                              value: visibility == user['uid']
+                                  ? 'Only me'
+                                  : visibility == 'public'
+                                      ? 'Everyone'
+                                      : visibility,
+                              onChanged: (dynamic value) {
+                                handleVisibility(value as String);
+                              },
+                            );
+                          },
+                        ),
+                        if (visibility == user['uid'])
+                          CustomButton(
+                            size: ButtonSize.small,
+                            icon: IconType.info,
+                            onPressed: () => {
+                              Modal.show(
+                                context,
+                                'Private Challenges',
+                                'Check your profile to see your challenges. Public challenges are displayed on the home screen.',
+                              ),
+                            },
+                          ),
+                      ],
+                    )
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
-      ],
+      ),
     );
   }
 }
